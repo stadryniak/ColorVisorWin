b427f956b16bde39cb610608ccad7148c42e2776
Michał Stadryniak <stadryniak.michal@gmail.com>
2020-03-20 16:41:04 +0100

    basic functionality done

--- ColorVisor/MainWindow.xaml.cs
+++ ColorVisor/MainWindow.xaml.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Color = System.Drawing.Color;
+using Point = System.Drawing.Point;
+using Timer = System.Timers.Timer;
 
 namespace ColorVisor
 {
@@ -20,18 +18,77 @@ namespace ColorVisor
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto, SetLastError = true, ExactSpelling = true)]
+        public static extern int BitBlt(IntPtr hDC, int x, int y, int nWidth, int nHeight, IntPtr hSrcDC, int xSrc,
+            int ySrc, int dwRop);
+
+        private readonly Timer timer;
+
         public MainWindow()
         {
-            InitializeComponent();
-            SetText();
-        }
+            ResizeMode = ResizeMode.NoResize;
+            SizeToContent = SizeToContent.WidthAndHeight;
 
-        private void SetText()
-        {
-            myText.Text = "HELLO WORLD";
+            InitializeComponent();
             myText.TextWrapping = TextWrapping.Wrap;
             myText.IsReadOnly = true;
             myText.BorderThickness = new Thickness(0);
+            myText.FontSize = 20;
+
+            timer = new Timer(100)
+            {
+                AutoReset = true,
+                Enabled = true
+            };
+            timer.Elapsed += MouseCheck;
+        }
+
+
+        private void MouseCheck(object sender, ElapsedEventArgs elapsedEventArgs)
+        {
+            Point cursor = new Point();
+            GetCursorPos(ref cursor);
+            var c = GetColorAt(cursor);
+            SetText(c);
+            SetBackground(c);
+        }
+
+        readonly Bitmap screenPixel = new Bitmap(1, 1);
+
+        public Color GetColorAt(Point location)
+        {
+            using (Graphics gdest = Graphics.FromImage(screenPixel))
+            {
+                using Graphics gsrc = Graphics.FromHwnd(IntPtr.Zero);
+                IntPtr hSrcDC = gsrc.GetHdc();
+                IntPtr hDC = gdest.GetHdc();
+                int retval = BitBlt(hDC, 0, 0, 1, 1, hSrcDC, location.X, location.Y, (int)CopyPixelOperation.SourceCopy);
+                gdest.ReleaseHdc();
+                gsrc.ReleaseHdc();
+            }
+            return screenPixel.GetPixel(0, 0);
+        }
+
+        private void SetText(Color mColor)
+        {
+            Dispatcher.Invoke(() =>
+                {
+                    myText.Text = mColor.R + " " + mColor.G + " " + mColor.B;
+                }
+            );
+        }
+
+        private void SetBackground(Color color)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                System.Windows.Media.Color mColor = System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B);
+                Background = new SolidColorBrush(mColor);
+                myText.Background = new SolidColorBrush(mColor);
+            });
         }
     }
 }
